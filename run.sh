484279a57b6f8883ef8d9f8a6b48f48e3802584a
jordan-lumley <57803850+jordan-lumley@users.noreply.github.com>
2022-11-08 16:57:44 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 while read LINE
 do
-    if [ ! -z "${LINE-}" ]
-    then
-        curl -X POST --silent --data-urlencode \
-            "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" $SLACK_WEBHOOK_URL
-        
-        echo $LINE
-    fi
+    PAYLOAD=$(jq --null-input --arg text "$LINE" '{"text": $text}')
+
+    curl --silent -X POST $SLACK_WEBHOOK_URL -H "Content-type:application/json" -d "$PAYLOAD" > /dev/null
+
+    echo $LINE
 done < "${1:-/dev/stdin}"
